6346014a726308cbafe2da769c49bd742d4a21b2
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-03-01 20:35:03 +0000

    Successfully read in test file and parsed the words from each line.
    Co-Authored by: Alena Davis <alenad@gmail.com>

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,19 @@
-# put your code here.
+# open file 
+# read in file by line
+# parse each line - for each line: split by spaces
+#   for each word in the line: check if it is in the dictionary and count up
+# return or print the wordcount at the end
+
+def count_words_dict(filename):
+        the_file = open(filename)
+        for line in the_file:
+            line = line.strip('\n')
+            words = line.split(' ')
+            print(line) ###
+            print(words)
+        # word_counts: {}
+        # for word in phrase:
+        #     word[letter] = letter_counts.get(letter, 0) + 1
+        # return word
+
+count_words_dict('test.txt')
